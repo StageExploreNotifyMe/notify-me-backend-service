3ca0d6f779a3381d95384069ef17d9a130080711
Gerd Goemans <gerd.goemans@xplore.be>
2021-04-20 09:30:17 +0200

    Project Setup

---
+++ src/test/java/be/xplore/notify/me/NotifymeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.xplore.notify.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
