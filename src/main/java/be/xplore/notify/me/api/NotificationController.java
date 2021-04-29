700777194cc3c212bf4692fd67f869b61796c599
jana.wouters <jana.wouters@student.kdg.be>
2021-04-29 12:50:28 +0200

    changed modelMapper to NotificationDtoMapper

--- src/main/java/be/xplore/notify/me/api/NotificationController.java
+++ src/main/java/be/xplore/notify/me/api/NotificationController.java
@@ -2,8 +2,8 @@ package be.xplore.notify.me.api;
 
 import be.xplore.notify.me.domain.notification.Notification;
 import be.xplore.notify.me.dto.NotificationDto;
+import be.xplore.notify.me.dto.mappers.NotificationDtoMapper;
 import be.xplore.notify.me.services.notification.NotificationService;
-import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
@@ -18,17 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class NotificationController {
     private final NotificationService notificationService;
-    private final ModelMapper modelMapper;
+    private final NotificationDtoMapper notificationDtoMapper;
 
-    public NotificationController(NotificationService notificationService, ModelMapper modelMapper) {
+    public NotificationController(NotificationService notificationService, NotificationDtoMapper notificationDtoMapper) {
         this.notificationService = notificationService;
-        this.modelMapper = modelMapper;
+
+        this.notificationDtoMapper = notificationDtoMapper;
     }
 
     @GetMapping("/inbox/{userId}/pending/{page}")
     public ResponseEntity<Page<NotificationDto>> getUserNotification(@PathVariable String userId, @PathVariable int page) {
         Page<Notification> notifications = notificationService.getAllNotifications(userId, PageRequest.of(page, 20));
-        Page<NotificationDto> notificationDtos = notifications.map(notification -> modelMapper.map(notification, NotificationDto.class));
+                Page<NotificationDto> notificationDtos = notifications.map(notificationDtoMapper::toDto);
         return new ResponseEntity<>(notificationDtos, HttpStatus.OK);
     }
 
