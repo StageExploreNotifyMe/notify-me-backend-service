f3ce9ddffa66cda1f7a7495392d88e64f95074a6
Gerd Goemans <gerd.goemans@xplore.be>
2021-05-03 09:57:10 +0200

    Resolved merge conflicts

--- src/main/java/be/xplore/notify/me/services/notification/NotificationService.java
+++ src/main/java/be/xplore/notify/me/services/notification/NotificationService.java
@@ -1,27 +1,28 @@
 package be.xplore.notify.me.services.notification;
 
+import be.xplore.notify.me.domain.exceptions.DatabaseException;
 import be.xplore.notify.me.domain.notification.Notification;
 import be.xplore.notify.me.entity.mappers.EntityMapper;
 import be.xplore.notify.me.entity.notification.NotificationEntity;
+import be.xplore.notify.me.repositories.NotificationRepo;
 import be.xplore.notify.me.services.RepoService;
 import be.xplore.notify.me.services.user.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Slf4j
 @Service
 public class NotificationService extends RepoService<Notification, NotificationEntity> {
 
     private final UserService userService;
+    private final NotificationRepo notificationRepo;
 
-    public NotificationService(JpaRepository<NotificationEntity, String> repo, EntityMapper<NotificationEntity, Notification> entityMapper, UserService userService) {
+    public NotificationService(NotificationRepo repo, EntityMapper<NotificationEntity, Notification> entityMapper, UserService userService) {
         super(repo, entityMapper);
         this.userService = userService;
+        this.notificationRepo = repo;
     }
 
     public Notification saveNotificationAndSendToInbox(Notification notification) {
@@ -33,7 +34,7 @@ public class NotificationService extends RepoService<Notification, NotificationE
     public Page<Notification> getAllNotifications(String userId, PageRequest pageRequest) {
         try {
             Page<NotificationEntity> notifications = notificationRepo.getAllByUserId(userId, pageRequest);
-            return notifications.map(notificationEntityMapper::fromEntity);
+            return notifications.map(entityMapper::fromEntity);
         } catch (Exception e) {
             log.error("Fetching all notifications for userId: {} failed: {}: {}", userId, e.getClass().getSimpleName(), e.getMessage());
             throw new DatabaseException(e);
