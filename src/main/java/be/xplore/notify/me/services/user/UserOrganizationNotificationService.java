1a4a92037e0daa5b792af41e1d09a248fe8f975e
jana.wouters <jana.wouters@student.kdg.be>
2021-05-04 08:54:07 +0200

    added notification for role change user

--- src/main/java/be/xplore/notify/me/services/user/UserOrganizationNotificationService.java
+++ src/main/java/be/xplore/notify/me/services/user/UserOrganizationNotificationService.java
@@ -4,11 +4,14 @@ import be.xplore.notify.me.domain.notification.Notification;
 import be.xplore.notify.me.domain.notification.NotificationType;
 import be.xplore.notify.me.domain.notification.NotificationUrgency;
 import be.xplore.notify.me.domain.user.MemberRequestStatus;
+import be.xplore.notify.me.domain.user.Role;
 import be.xplore.notify.me.domain.user.User;
 import be.xplore.notify.me.domain.user.UserOrganization;
 import be.xplore.notify.me.services.notification.NotificationSenderService;
 import be.xplore.notify.me.services.notification.NotificationService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserOrganizationNotificationService {
@@ -46,6 +49,28 @@ public class UserOrganizationNotificationService {
 
         builder.title(title);
         builder.body(body);
+    }
+
+    public void sendOrganizationRoleChangeNotification(UserOrganization userOrganization) {
+        User user = userOrganization.getUser();
+        Notification.NotificationBuilder builder = Notification.builder().userId(user.getId());
+        builder.title("Changed member role");
+        builder.body(String.format("The organization changed your member role to %s", userOrganization.getRole()));
+        if (userOrganization.getRole().equals(Role.MEMBER)) {
+            builder.type(NotificationType.USER_DEMOTED);
+        } else {
+            builder.type(NotificationType.USER_PROMOTED);
+        }
+        setChangedRoleNotificationDetails(user, builder);
+    }
+
+    private void setChangedRoleNotificationDetails(User user, Notification.NotificationBuilder builder) {
+        builder.usedChannel(user.getUserPreferences().getNormalChannel());
+        builder.creationDate(LocalDateTime.now());
+        builder.urgency(NotificationUrgency.NORMAL);
+        Notification notification = builder.build();
+        notificationService.saveNotificationAndSendToInbox(notification);
+        notificationSenderService.sendNotification(notification);
     }
 
 }
--- src/main/java/be/xplore/notify/me/services/user/UserOrganizationService.java
+++ src/main/java/be/xplore/notify/me/services/user/UserOrganizationService.java
@@ -90,6 +90,7 @@ public class UserOrganizationService {
                 .user(userOrganization.getUser())
                 .role(roleToChangeTo)
                 .build();
+        userOrganizationNotificationService.sendOrganizationRoleChangeNotification(updated);
 
         return save(updated);
     }
