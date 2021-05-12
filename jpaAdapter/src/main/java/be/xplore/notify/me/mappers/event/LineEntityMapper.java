42372e69afb3c0d0b9c54f111e4e5db4020b465e
Gerd Goemans <gerd.goemans@xplore.be>
2021-05-12 10:08:51 +0200

    Added number of required users to a line

--- jpaAdapter/src/main/java/be/xplore/notify/me/mappers/event/LineEntityMapper.java
+++ jpaAdapter/src/main/java/be/xplore/notify/me/mappers/event/LineEntityMapper.java
@@ -22,6 +22,7 @@ public class LineEntityMapper implements EntityMapper<LineEntity, Line> {
                 .name(lineEntity.getName())
                 .description(lineEntity.getDescription())
                 .venue(venueEntityMapper.fromEntity(lineEntity.getVenueEntity()))
+                .numberOfRequiredPeople(lineEntity.getNumberOfRequiredPeople())
                 .build();
     }
 
@@ -31,7 +32,8 @@ public class LineEntityMapper implements EntityMapper<LineEntity, Line> {
                 line.getId(),
                 line.getName(),
                 line.getDescription(),
-                venueEntityMapper.toEntity(line.getVenue())
+                venueEntityMapper.toEntity(line.getVenue()),
+                line.getNumberOfRequiredPeople()
         );
     }
 }
