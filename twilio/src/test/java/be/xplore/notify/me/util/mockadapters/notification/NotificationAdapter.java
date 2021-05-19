e101ba8aade0f470fed25686a82f6876b8097691
Gerd Goemans <gerd.goemans@xplore.be>
2021-05-19 11:19:02 +0200

    Added missing methods

--- twilio/src/test/java/be/xplore/notify/me/util/mockadapters/notification/NotificationAdapter.java
+++ twilio/src/test/java/be/xplore/notify/me/util/mockadapters/notification/NotificationAdapter.java
@@ -1,11 +1,13 @@
 package be.xplore.notify.me.util.mockadapters.notification;
 
 import be.xplore.notify.me.domain.notification.Notification;
+import be.xplore.notify.me.domain.notification.NotificationType;
 import be.xplore.notify.me.persistence.NotificationRepo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -28,6 +30,31 @@ public class NotificationAdapter implements NotificationRepo {
 
     @Override
     public Page<Notification> getAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<Notification> getAllByNotificationType(NotificationType notificationType, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<Notification> getAllByEventId(String eventId, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<Notification> getAllByTypeAndEvent(NotificationType notificationType, String eventId, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<String> getAllEventIds() {
+        return null;
+    }
+
+    @Override
+    public List<Object[]> getChannelAmount() {
         return null;
     }
 }
